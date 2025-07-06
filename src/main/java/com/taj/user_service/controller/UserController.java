962b737f80a7423d262d0e000a4a8292244db71f
priyanshu0047 <thakurpriyanshusingh44gmail.com>
2025-07-06 15:54:08 +0530

    add new feature getusers()

--- src/main/java/com/taj/user_service/controller/UserController.java
+++ src/main/java/com/taj/user_service/controller/UserController.java
@@ -3,14 +3,17 @@ package com.taj.user_service.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.taj.user_service.modal.User;
-import com.taj.user_service.security.UserService;
+import com.taj.user_service.service.UserService;
 
 import ch.qos.logback.core.status.Status;
 import jakarta.validation.Valid;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +32,8 @@ public class UserController {
 		return new ResponseEntity<User>(userService.setUser(user), HttpStatusCode.valueOf(200)) ;
 	}
 	
-
+	@GetMapping("/getusers")
+	public ResponseEntity<List<User>> getUsers() {
+		return new ResponseEntity<List<User>>(userService.getUsers(), HttpStatusCode.valueOf(200));
+	}
 }
--- src/main/java/com/taj/user_service/service/UserService.java
+++ src/main/java/com/taj/user_service/service/UserService.java
@@ -1,4 +1,6 @@
-package com.taj.user_service.security;
+package com.taj.user_service.service;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +17,8 @@ public class UserService {
 	public User setUser(User user) {
 		return userRepo.save(user);
 	}
-
+      
+	public List<User> getUsers() {
+		return userRepo.findAll();
+	}
 }
