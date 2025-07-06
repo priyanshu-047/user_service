694386c78ad1a69cb194b6797f2d997219ef122d
priyanshu0047 <thakurpriyanshusingh44gmail.com>
2025-07-06 15:39:07 +0530

    runing succesfully   user-service/adduser/

--- src/main/java/com/taj/user_service/modal/User.java
+++ src/main/java/com/taj/user_service/modal/User.java
@@ -1,10 +1,9 @@
 package com.taj.user_service.modal;
 
-import org.antlr.v4.runtime.misc.NotNull;
+
 import org.springframework.format.annotation.NumberFormat;
 import org.springframework.format.annotation.NumberFormat.Style;
 
-import jakarta.annotation.Nonnull;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -29,6 +28,36 @@ public class User {
 	private String password;
     @NumberFormat(style = Style.NUMBER)
 	private int phoneNumber;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 	
 	
 }
